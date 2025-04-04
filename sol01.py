82e9b4a3381a22dc36f918c672ed9fa99fa870d3
Roi Daradal <roidaradal@gmail.com>
2025-04-04 22:41:16 +0800

    v0.1.3 - Day 01 Part 2 - Python

--- sol01.py
+++ sol01.py
@@ -1,5 +1,8 @@
 from utils import *
 
+# SolutionA:    24000   70698
+# SolutionB:    45000   206643
+
 def input01(full: bool) -> list[str]:
     return readFile(getPath(1, full))
 
@@ -15,5 +18,23 @@ def day01A():
     maxCalories = max(maxCalories, current)
     print("MaxCalories:", maxCalories)
 
+def day01B():
+    full = True 
+    top3, current = [0,0,0], 0
+    for line in input01(full):
+        if line == "":
+            top3 = adjustTop3(top3, current)
+            current = 0
+        else:
+            current += int(line)
+    top3 = adjustTop3(top3, current)
+    print("Total:", sum(top3))
+
+def adjustTop3(top3: list[int], current: int) -> list[int]:
+    top3.append(current)
+    top3.sort()
+    return top3[1:4]
+
 if __name__ == '__main__':
-    day01A()
+    # day01A()
+    day01B()
