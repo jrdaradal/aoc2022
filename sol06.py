38a4d9faf099c90bdb54d8a5130c79d77742861d
Roi Daradal <roidaradal@gmail.com>
2025-04-05 12:41:43 +0800

    v0.6.3 - Day 06 Part 2 - Python

--- sol06.py
+++ sol06.py
@@ -1,6 +1,7 @@
 from utils import *
 
 # SolutionA: 7,5,6,10,11    1140
+# SolutionB: 19,23,23,29,26	3495
 
 def input06(full: bool) -> list[str]:
     return readFile(getPath(6, full))
@@ -8,16 +9,22 @@ def input06(full: bool) -> list[str]:
 def day06A():
     full = True 
     for line in input06(full):
-        print("Marker:", findMarker(line))
+        print("Marker:", findMarker(line, 4))
 
-def findMarker(line: str) -> int:
+def day06B():
+    full = True 
+    for line in input06(full):
+        print("Marker:", findMarker(line, 14))
+
+def findMarker(line: str, numUnique: int) -> int:
     count = len(line)
-    for n in range(4,count+1):
-        if allUnique(line[n-4:n]):
+    for n in range(numUnique,count+1):
+        if allUnique(line[n-numUnique:n]):
             return n 
 
 def allUnique(text: str) -> bool:
     return len(text) == len(set(text))
 
 if __name__ == '__main__':
-    day06A()
+    # day06A()
+    day06B()
